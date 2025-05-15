8f16446b3f4b0fc5d86ea1bbba522f066c75c877
Yudzxml <yudaaryaardhana1122@gmail.com>
2025-05-15 23:16:41 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
+import express from 'express';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-fs.readdirSync(path.join(__dirname, 'api')).forEach((file) => {
+fs.readdirSync(path.join(__dirname, 'api')).forEach(async (file) => {
   const routeName = file.replace('.js', '');
   const routePath = `/api/${routeName}`;
-  const routeModule = require(`./api/${file}`);
+  const { default: routeModule } = await import(`./api/${file}`);
   app.use(routePath, routeModule);
 });
 
 app.get('/', (req, res) => {
-  res.send('Multi API Server Aktif');
+  res.send('Multi API Server Aktif (ESM)');
 });
 
 app.listen(PORT, () => {
