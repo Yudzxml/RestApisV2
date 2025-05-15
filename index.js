8d158be334974f787fb50e69ec7b1a52d44bc7eb
Yudzxml <yudaaryaardhana1122@gmail.com>
2025-05-16 05:53:48 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+import express from 'express';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
